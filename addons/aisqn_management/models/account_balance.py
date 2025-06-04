e7cd6bb7e368e34b89016e7acc052f03d38109be
Phat <kieuthanhphat.work@gmail.com>
2025-06-04 11:27:07 +0700

    fix view

--- addons/aisqn_management/models/account_balance.py
+++ addons/aisqn_management/models/account_balance.py
@@ -4,7 +4,7 @@ class AccountBalance(models.Model):
     _name = 'uni.invest.account_balance'
     _description = 'Account Balance'
 
-    user_id = fields.Many2one('uni.invest.user', string='User', required=True)
+    user_id = fields.Many2one('uni.user', string='User', required=True)
     amount = fields.Float(string='Amount', default=0.0)
     pending_amount_withdrawal_invest = fields.Float(string='Pending Amount Withdrawal Invest', default=0.0)
     pending_amount_withdrawal_profit = fields.Float(string='Pending Amount Withdrawal Profit', default=0.0)
@@ -14,9 +14,9 @@ class AccountBalance(models.Model):
         ('inactive', 'Inactive'),
         ('pending', 'Pending'),
     ], string='Status', default='active')
-    currency_id = fields.Many2one('res.currency', string='Currency')
-    created_date_time = fields.Datetime(string='Created Date Time', default=fields.Datetime.now)
-    created_user_id = fields.Many2one('uni.invest.user', string='Created By')
+    # currency_id = fields.Many2one('res.currency', string='Currency')
+    # created_date_time = fields.Datetime(string='Created Date Time', default=fields.Datetime.now)
+    # created_user_id = fields.Many2one('uni.user', string='Created By')
     available_balance = fields.Float(string='Available Balance', compute='_compute_available_balance', store=True)
 
     @api.depends('amount', 'pending_amount_withdrawal_invest', 'pending_amount_withdrawal_profit', 'pending_amount')
